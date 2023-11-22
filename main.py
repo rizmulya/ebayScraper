46365a599b058a004c162c661bfcac54baf61945
Risky Mulya <122626344+rizmulya@users.noreply.github.com>
2023-11-22 07:01:51 +0700

    231120

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Spiders.ebay import EbaySpider
 spider = EbaySpider()
 spider.exec(
     search_keywords = "book", 
-    page_to_scrape = 3, #upto166
+    pages_to_scrape = 3, #number of pages to scrape (up to 166)
     output_csv = "Reports/ebay_data.csv",
     output_json = "Reports/ebay_data.json"
 )
